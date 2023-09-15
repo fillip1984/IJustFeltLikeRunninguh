59917eecf33efe0b6c440e296f4e2a51d406b611
fillip1984 <fillip1984@gmail.com>
2023-09-15 17:18:22 -0400

    Initial Commit

---
+++ IJustFeltLikeRunninguh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IJustFeltLikeRunninguh
+//
+//  Created by Phillip Williams on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IJustFeltLikeRunninguh/IJustFeltLikeRunninguhApp.swift
@@ -0,0 +1,17 @@
+//
+//  IJustFeltLikeRunninguhApp.swift
+//  IJustFeltLikeRunninguh
+//
+//  Created by Phillip Williams on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct IJustFeltLikeRunninguhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
